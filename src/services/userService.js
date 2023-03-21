796672f9ebac50c3c9b5e9e1c7b5f268d7cc5ba5
arnh8 <arnold.hsieh8@gmail.com>
2023-03-21 14:59:03 -0400

    cleanup logging

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 const baseUrl =
-  // "https://ec2-100-26-159-248.compute-1.amazonaws.com:6969/api/v1/";
-  "https://localhost:6969/api/v1/";
+  "https://ec2-100-26-159-248.compute-1.amazonaws.com:6969/api/v1/";
+//"https://localhost:6969/api/v1/";
 
 const login = async (credentials) => {
+  console.log("Im logging in to " + baseUrl + "with " + credentials.email);
   const response = await axios.post(baseUrl + "login", credentials);
   return response.data;
 };
 
 const register = async (credentials) => {
-  console.log(credentials);
-  console.log("im posting to " + baseUrl)
+  console.log("Im registering to " + baseUrl + "with " + credentials.email);
   const response = await axios.post(baseUrl + "register", credentials);
   return response.data;
 };
