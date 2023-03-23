4e34033f3df97bb6df50192e134d8797efe0e09c
arnh8 <arnold.hsieh8@gmail.com>
2023-03-23 16:55:33 -0400

    proper background

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -6,10 +6,11 @@ import Register from "../modules/Register";
 import Calendar from "../modules/Calendar";
 import "../styles/App.css";
 import RedirectToAppropriate from "../modules/Redirect";
+import rose from "../assets/rose-petals.svg";
 
 function App() {
   const [user, setUser] = useState(0);
-  const [userData, setUserData] = useState([]);
+  const [userData, setUserData] = useState([{ date: new Date(), hours: "8" }]);
   //Userdata is an array of days... {}
   const registerHandler = async (credentials) => {
     userService.register(credentials);
@@ -24,22 +25,34 @@ function App() {
 
   const submitUserData = (data) => {};
 
+  const backgroundStyle = {
+    backgroundImage: `url(${rose})`,
+    height: "100vh",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    "background-position": "center center",
+  };
+
   return (
-    <BrowserRouter>
-      <div className="App"></div>
-      <Routes>
-        <Route path="/" element={<RedirectToAppropriate />} />
-        <Route path="/login" element={<Login loginHandler={loginHandler} />} />
-        <Route
-          path="/register"
-          element={<Register registerHandler={registerHandler} />}
-        />
-        <Route
-          path="/calendar"
-          element={<Calendar setUserData={setUserData} />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <div className="App" style={backgroundStyle}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RedirectToAppropriate />} />
+          <Route
+            path="/login"
+            element={<Login loginHandler={loginHandler} />}
+          />
+          <Route
+            path="/register"
+            element={<Register registerHandler={registerHandler} />}
+          />
+          <Route
+            path="/calendar"
+            element={<Calendar setUserData={setUserData} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
