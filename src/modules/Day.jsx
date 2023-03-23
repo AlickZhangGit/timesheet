4627ee7bd004a238a185d02d9674e1f4a028e2cc
AlickZhangGit <alickzhang1997@gmail.com>
2023-03-23 16:46:50 -0400

    calendar page css

--- src/modules/Day.jsx
+++ src/modules/Day.jsx
@@ -4,7 +4,13 @@ export default function Day(props) {
   //takes a number as a prop
   //console.log(props["data-key"]);
   return (
-    <div className={props.className + " day"} data-key={props["data-key"]}>
+    <div
+      className={props.className + " day"}
+      data-key={props["data-key"]}
+      style={{
+        display: "inline-block",
+      }}
+    >
       {props.date.getDate()}
     </div>
   );
