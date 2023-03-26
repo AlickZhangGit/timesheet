fa0e3874886c1ec8ba80e024b832029e1bf9f2a1
AlickZhangGit <alickzhang1997@gmail.com>
2023-03-25 22:27:39 -0400

    double click modal

--- src/controllers/queries.ts
+++ src/controllers/queries.ts
@@ -4,4 +4,5 @@ export const queries = {
     addUser: 'INSERT INTO timesheet.users (email,password) VALUES ($1, $2);',
     getUser: 'Select email, password FROM timesheet.users WHERE email = $1',
     getTimeByMonth: 'SELECT * FROM timesheet.times WHERE email = $1 AND year = $2 AND month = $3;',
+    insertTimes: 'INSERT INTO timesheet.times (email, year, month, day, hours) VALUES $1 ON CONFLICT (email, year, month, day) DO UPDATE SET hours = excluded.hours;' ,
 }
--- src/middleware/authJwt.ts
+++ src/middleware/authJwt.ts
@@ -5,7 +5,8 @@ dotenv.config({ path: './.env'})
 
 // verify token
 const verifyToken = (req, res, next) => {
-    const token = req.cookies?.access_token;
+    // const token = req.cookies?.access_token;
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImV4YW1wbGVAZXhhbXBsZS5jb20iLCJpYXQiOjE2Nzk3MDYyNzEsImV4cCI6MTc2NjEwNjI3MX0.tMCgARoNUkGG6kPDnHGMWd6yiQMbPk0hJUFy7iNLf0k'
   
     if (!token) {
         return res.status(403).send({ message: "No token provided!" });
