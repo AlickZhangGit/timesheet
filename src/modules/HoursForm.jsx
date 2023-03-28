4edff234161ff7270d3099868e92d86740e5890a
arnh8 <arnold.hsieh8@gmail.com>
2023-03-28 19:55:21 -0400

    add edit all function

--- src/modules/HoursForm.jsx
+++ src/modules/HoursForm.jsx
@@ -2,16 +2,26 @@ import { useEffect, useState } from "react";
 import userService from "../services/userService";
 import "../styles/HoursForm.css";
 
+function init(selectedDays) {
+  const obj = {};
+  for (let i = 0; i < selectedDays.length; i++) {
+    obj[selectedDays[i]] = "";
+  }
+  console.log(obj);
+  return obj;
+}
+
 export default function HoursForm({ selectedDays, closeModal, updateDaysArr }) {
   const [errmessage, setErrMessage] = useState("");
   const [linkAllInput, setLinkAllInput] = useState(false);
   const [linkedVal, setLinkedVal] = useState("");
+  const [allVals, setAllVals] = useState(init(selectedDays));
   //Takes an array of days (just ids of dates, they need to be cast as Dates)
   const handleSubmit = async (event) => {
     event.preventDefault();
     const arr = [];
     const els = event.target;
-    for (let i = 0; i < els.length - 1; i++) {
+    for (let i = 1; i < els.length - 1; i++) {
       const date = new Date(event.target[i].getAttribute("data-key"));
       const hours = event.target[i].value;
       if (hours === "") {
@@ -34,6 +44,31 @@ export default function HoursForm({ selectedDays, closeModal, updateDaysArr }) {
     setLinkAllInput(event.target.checked);
   };
 
+  const handleGenericChange = (event) => {
+    const value = event.target.value;
+    //console.log(allVals);
+    if (linkAllInput) {
+      /*setAllVals(
+        Object.keys(allVals).forEach((key) => {
+          allVals;
+        })
+      );*/
+      //Create a clone of the object:
+      const copy = Object.assign({}, allVals);
+
+      Object.keys(copy).forEach((key) => {
+        return (copy[key] = value);
+      });
+
+      setAllVals(copy);
+      return;
+    }
+    setAllVals({
+      ...allVals,
+      [event.target.name]: value,
+    });
+  };
+
   return (
     <form onSubmit={handleSubmit} className="hoursForm">
       <label htmlFor="linkCheckbox">
@@ -46,14 +81,12 @@ export default function HoursForm({ selectedDays, closeModal, updateDaysArr }) {
         />
       </label>
       {selectedDays.map((day) => {
-        console.log(day);
         return (
           <DayEntry
             day={day}
             key={day}
-            linkedVal={linkedVal}
-            setLinkedVal={setLinkedVal}
-            linkAllInput={linkAllInput}
+            handleGenericChange={handleGenericChange}
+            allVals={allVals}
           />
         );
       })}
@@ -69,8 +102,8 @@ export default function HoursForm({ selectedDays, closeModal, updateDaysArr }) {
   );
 }
 
-function DayEntry({ day, linkedVal, setLinkedVal, linkAllInput }) {
-  const [hours, setHours] = useState(linkedVal);
+function DayEntry({ day, handleGenericChange, allVals }) {
+  const [hours, setHours] = useState("");
   const date = new Date(day);
   const options = {
     weekday: "short",
@@ -82,7 +115,6 @@ function DayEntry({ day, linkedVal, setLinkedVal, linkAllInput }) {
 
   const handleChange = (event) => {
     if (linkAllInput) {
-      console.log("Wa");
       setLinkedVal(event.target.value);
     }
     setHours(event.target.value);
@@ -96,10 +128,18 @@ function DayEntry({ day, linkedVal, setLinkedVal, linkAllInput }) {
       <input
         className="formEntryInput"
         type="text"
-        value={hours}
-        onChange={handleChange}
+        name={day}
+        value={allVals[day]}
+        onChange={handleGenericChange}
         data-key={date.toLocaleDateString(undefined, options)}
       />{" "}
     </label>
   );
 }
+
+/*
+linkedVal={linkedVal}
+            setLinkedVal={setLinkedVal}
+            linkAllInput={linkAllInput} 
+            linkedVal, setLinkedVal, linkAllInput
+            */
