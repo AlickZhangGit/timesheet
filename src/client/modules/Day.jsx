6a025dc6cfc4ae88757c2f8ddad76e5994957cd0
arnh8 <arnold.hsieh8@gmail.com>
2023-04-05 23:07:55 -0400

    cleanup unused code

--- src/client/modules/Day.jsx
+++ src/client/modules/Day.jsx
@@ -4,17 +4,6 @@ import "../styles/Day.css";
 
 export default function Day(props) {
   const [hourData, setHoursData] = useState(false);
-  const [showHourDetails, setShowHourDetails] = useState(false);
-
-  function handleCellDoubleClick(event) {
-    setShowHourDetails(!showHourDetails);
-    const { target } = event;
-    const { offsetTop, offsetLeft } = target;
-    const x = offsetLeft + target.offsetWidth / 2;
-    const y = offsetTop + target.offsetHeight / 2;
-    setModalPosition({ x, y });
-    setIsOpen(true);
-  }
 
   function dotdotdot(hours) {
     const stringed = String(hours);
@@ -32,7 +21,6 @@ export default function Day(props) {
       className={props.className + " day"}
       data-key={props["data-key"]}
       id={"modal-root" + props["data-key"]}
-      onDoubleClick={handleCellDoubleClick}
     >
       <div className="dayDate">{props.dayObj.date.getDate()}</div>
 
